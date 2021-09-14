3bf51268e47d2916c78307b0054636e9e2c1ee03
miyasan_0301 <miyasan.19990301@gmail.com>
2021-09-14 23:08:31 +0900

    config:env-file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,34 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
-import { BooksModule } from './books/books.module';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Book } from './books/book';
+import { BooksModule } from './books/books.module';
+import { TypeOrmConfigService } from './config/typeorm-config.service';
 
 @Module({
   imports: [
+    /** env読み込み
+     *   環境変数NODE_ENVの値によって読み込むファイルを切り替える。
+     *   default.envは後続で呼ばれる。同じ変数がある場合は先に定義されているものが優先される。
+     */
+    ConfigModule.forRoot({
+      envFilePath: [`.env/${process.env.NODE_ENV}.env`],
+      isGlobal: true,
+      // ignoreEnvFile: true, // <- 環境変数から取得する場合はコメントアウトを外す．
+    }),
+
     // graphqlのモジュールをアプリ全体に依存性注入
     GraphQLModule.forRoot({
       // DTOを検知して、スキーマ自動生成
       autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'miyashota',
-      password: '0301',
-      database: 'nesttodo',
-      entities: [Book],
-      synchronize: true,
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
     }),
+
     // （BooksModuleはAppModuleに依存する）
     BooksModule,
   ],
---
+++ src/config/typeorm-config.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  // TypeORMの設定
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    const configService = new ConfigService();
+    return {
+      type: 'postgres',
+      host: configService.get<string>('DATABASE_HOSTNAME'),
+      port: Number(configService.get<number>('DATABASE_PORT')),
+      username: configService.get<string>('DATABASE_USERNAME'),
+      password: configService.get<string>('DATABASE_PASSWORD'),
+      database: configService.get<string>('DATABASE_NAME'),
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      synchronize: this.strToBoolean(
+        configService.get<string>('DATABASE_SYNC', 'false'),
+      ),
+
+      // herokuデプロイ時に必要
+      // ssl:
+      //   process.env.NODE_ENV === 'production'
+      //     ? { rejectUnauthorized: false }
+      //     : true,
+    };
+  }
+  // get<boolean>が上手く変換してくれないため泣く泣く対応
+  private strToBoolean(boolStr: string): boolean {
+    switch (boolStr.toLowerCase().trim()) {
+      case 'true':
+      case 'yes':
+      case '1':
+        return true;
+      case 'false':
+      case 'no':
+      case '0':
+      case null:
+        return false;
+      default:
+        return boolStr as unknown as boolean;
+    }
+  }
+}
