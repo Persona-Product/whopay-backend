a737b7d47eeed83f263c9f4e3a4971693756541e
miyasan_0301 <miyasan.19990301@gmail.com>
2021-09-19 14:29:17 +0900

    remove:book-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,15 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BooksModule } from './modules/books/books.module';
 import { TypeOrmConfigService } from './config/typeorm-config.service';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     /** env読み込み
-     *   環境変数NODE_ENVの値によって読み込むファイルを切り替える。
-     *   default.envは後続で呼ばれる。同じ変数がある場合は先に定義されているものが優先される。
+     *   環境変数NODE_ENVの値によって読み込むファイルを切り替える
+     *   default.envは後続で呼ばれる
+     *   同じ変数がある場合は先に定義されているものが優先される
      */
     ConfigModule.forRoot({
       envFilePath: [`.env/${process.env.NODE_ENV}.env`],
@@ -30,9 +30,7 @@ import { UsersModule } from './modules/users/users.module';
       useClass: TypeOrmConfigService,
     }),
 
-    // （BooksModuleはAppModuleに依存する）
-    BooksModule,
-
+    // これらのModuleはAppModuleに依存する
     UsersModule,
   ],
   controllers: [],
