25d1af2c2f7578761c96c1d5a75f3d27965f2fb7
miyasan_0301 <miyasan.19990301@gmail.com>
2021-09-25 23:34:24 +0900

    feat:foreign-key

--- src/modules/tweet/entity/tweet.entity.ts
+++ src/modules/tweet/entity/tweet.entity.ts
@@ -3,21 +3,27 @@ import {
   Entity,
   BaseEntity,
   Column,
-  PrimaryColumn,
   CreateDateColumn,
-  // PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  PrimaryGeneratedColumn,
+  OneToMany,
 } from 'typeorm';
+import { User } from '@/user/entity/user.entity';
+import { Retweet } from '@/retweet/entity/retweet.entity';
+import { Like } from '@/like/entity/like.entity';
+import { Comment } from '@/comment/entity/comment.entity';
 
 @Entity('tweet')
 @ObjectType()
 export class Tweet extends BaseEntity {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn('uuid')
   @Field()
   id: string;
 
-  @Column()
-  @Field()
-  userId: string;
+  @ManyToOne((type) => User, (user) => user.tweets)
+  @JoinColumn({ name: 'userId' })
+  userId: User;
 
   @Column()
   @Field()
@@ -26,4 +32,13 @@ export class Tweet extends BaseEntity {
   @CreateDateColumn()
   @Field()
   createdAt: Date;
+
+  @OneToMany((type) => Retweet, (retweet) => retweet.tweetId)
+  retweets: Retweet[];
+
+  @OneToMany((type) => Like, (like) => like.tweetId)
+  likes: Like[];
+
+  @OneToMany((type) => Comment, (comment) => comment.tweetId)
+  comments: Comment[];
 }
