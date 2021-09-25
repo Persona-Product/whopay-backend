3acb72edd43733f92021a0dc19bbd5e236fdb7f8
miyasan_0301 <miyasan.19990301@gmail.com>
2021-09-25 23:34:22 +0900

    feat:foreign-key

--- src/modules/retweet/entity/retweet.entity.ts
+++ src/modules/retweet/entity/retweet.entity.ts
@@ -2,26 +2,28 @@ import { Field, ObjectType } from '@nestjs/graphql';
 import {
   Entity,
   BaseEntity,
-  Column,
-  PrimaryColumn,
   CreateDateColumn,
-  // PrimaryGeneratedColumn,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+import { User } from '@/user/entity/user.entity';
+import { Tweet } from '@/tweet/entity/tweet.entity';
 
 @Entity('retweet')
 @ObjectType()
 export class Retweet extends BaseEntity {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn('uuid')
   @Field()
   id: string;
 
-  @Column()
-  @Field()
-  userId: string;
+  @ManyToOne((type) => User, (user) => user.tweets)
+  @JoinColumn({ name: 'userId' })
+  userId: User;
 
-  @Column()
-  @Field()
-  tweetId: string;
+  @ManyToOne((type) => Tweet, (tweet) => tweet.likes)
+  @JoinColumn({ name: 'tweetId' })
+  tweetId: Tweet;
 
   @CreateDateColumn()
   @Field()
