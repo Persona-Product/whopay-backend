3f8ab4f1bd8c951032c0271a20d21d7456afb695
miyasan_0301 <miyasan.19990301@gmail.com>
2021-10-01 23:43:52 +0900

    feat:follow-module

--- src/module/follow/follow.resolver.ts
+++ src/module/follow/follow.resolver.ts
@@ -1,4 +1,27 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Follow } from '@src/entity';
+import { FollowService } from '@follow/follow.service';
+import { CreateFollowDto } from '@follow/dto/create-follow.dto';
 
 @Resolver()
-export class FollowResolver {}
+export class FollowResolver {
+  constructor(private followService: FollowService) {}
+
+  // get all user, all follow
+  @Query((returns) => [Follow])
+  GetAllFollow() {
+    return this.followService.getAllFollow();
+  }
+
+  // create follow
+  @Mutation((returns) => Follow)
+  CreateFollow(@Args('followDto') createFollowDto: CreateFollowDto) {
+    return this.followService.createFollow(createFollowDto);
+  }
+
+  // // delete follow
+  @Mutation((returns) => Boolean)
+  DeleteFollow(@Args({ name: 'id', type: () => Int }) id: number) {
+    return this.followService.deleteFollow(id);
+  }
+}
--- src/module/follow/follow.service.ts
+++ src/module/follow/follow.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Follow } from '@src/entity';
+import { CreateFollowDto } from '@follow/dto/create-follow.dto';
 
 @Injectable()
-export class FollowService {}
+export class FollowService {
+  constructor(
+    @InjectRepository(Follow)
+    private followRepostiory: Repository<Follow>,
+  ) {}
+
+  // get all user, all follow
+  async getAllFollow(): Promise<Follow[]> {
+    const data = await this.followRepostiory.find();
+    return data;
+  }
+
+  // get tweet
+  async getUserFollow(id: string): Promise<Follow[]> {
+    return await this.followRepostiory.find({
+      userId: id,
+    });
+  }
+
+  // create follow
+  async createFollow(createFollowDto: CreateFollowDto): Promise<Follow> {
+    const follow = await this.followRepostiory.create(createFollowDto);
+    await this.followRepostiory.save(follow);
+    return follow;
+  }
+
+  // delete follow
+  async deleteFollow(id: number): Promise<boolean> {
+    const result = await this.followRepostiory.delete(id);
+    return result.affected > 0;
+  }
+}
