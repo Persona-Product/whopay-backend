59c9d14b5d51a145a1df648ee995fa56aa0a4006
miyasan_0301 <miyasan.19990301@gmail.com>
2021-10-01 23:43:35 +0900

    feat:comment-module

--- src/module/comment/comment.resolver.ts
+++ src/module/comment/comment.resolver.ts
@@ -1,4 +1,36 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { NotFoundException } from '@nestjs/common';
+import { Comment } from '@src/entity';
+import { CommentService } from '@comment/comment.service';
+import { CreateCommentDto } from '@comment/dto/create-comment.dto';
 
-@Resolver()
-export class CommentResolver {}
+@Resolver((of) => Comment)
+export class CommentResolver {
+  constructor(private commentService: CommentService) {}
+
+  // get all user, all tweet
+  @Query((returns) => [Comment])
+  GetAllComment() {
+    return this.commentService.getAllComment();
+  }
+
+  // get tweet
+  @Query((returns) => Comment)
+  GetOneComment(@Args({ name: 'id', type: () => Int }) id: number) {
+    const tweet = this.commentService.getOneComment(id);
+    if (!tweet) throw new NotFoundException(id);
+    return tweet;
+  }
+
+  // create tweet
+  @Mutation((returns) => Comment)
+  CreateComment(@Args('tweetDto') createCommentDto: CreateCommentDto) {
+    return this.commentService.createComment(createCommentDto);
+  }
+
+  // // delete tweet
+  @Mutation((returns) => Boolean)
+  DeleteComment(@Args({ name: 'id', type: () => Int }) id: number) {
+    return this.commentService.deleteComment(id);
+  }
+}
--- src/module/comment/comment.service.ts
+++ src/module/comment/comment.service.ts
@@ -1,4 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Comment } from '@src/entity';
+import { CreateCommentDto } from '@comment/dto/create-comment.dto';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  constructor(
+    @InjectRepository(Comment)
+    private commentRepostiory: Repository<Comment>,
+  ) {}
+
+  // get all user, all comment
+  async getAllComment(): Promise<Comment[]> {
+    const data = await this.commentRepostiory.find();
+    return data;
+  }
+
+  // get user, all comment
+  async getOneComment(id: number): Promise<Comment> {
+    return await this.commentRepostiory.findOne(id);
+  }
+
+  // get comment
+  async getUserComment(id: string): Promise<Comment[]> {
+    return await this.commentRepostiory.find({
+      userId: id,
+    });
+  }
+
+  // get comment
+  async getTweetComment(id: number): Promise<Comment[]> {
+    return await this.commentRepostiory.find({
+      tweetId: id,
+    });
+  }
+
+  // create comment
+  async createComment(createCommentDto: CreateCommentDto): Promise<Comment> {
+    const comment = await this.commentRepostiory.create(createCommentDto);
+    await this.commentRepostiory.save(comment);
+    return comment;
+  }
+
+  // delete comment
+  async deleteComment(id: number): Promise<boolean> {
+    const result = await this.commentRepostiory.delete(id);
+    return result.affected > 0;
+  }
+}
