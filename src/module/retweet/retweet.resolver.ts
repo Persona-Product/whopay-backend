826fb69336918e642874cf79c64065f1fb682c12
miyasan_0301 <miyasan.19990301@gmail.com>
2021-10-01 23:44:13 +0900

    feat:retweet-module

--- src/module/retweet/retweet.resolver.ts
+++ src/module/retweet/retweet.resolver.ts
@@ -1,4 +1,27 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Retweet } from '@src/entity';
+import { RetweetService } from '@retweet/retweet.service';
+import { CreateRetweetDto } from '@retweet/dto/create-retweet.dto';
 
-@Resolver()
-export class RetweetResolver {}
+@Resolver((of) => Retweet)
+export class RetweetResolver {
+  constructor(private retweetService: RetweetService) {}
+
+  // get all user, all retweet
+  @Query((returns) => [Retweet])
+  GetAllRetweet() {
+    return this.retweetService.getAllRetweet();
+  }
+
+  // create retweet
+  @Mutation((returns) => Retweet)
+  CreateRetweet(@Args('retweetDto') createRetweetDto: CreateRetweetDto) {
+    return this.retweetService.createRetweet(createRetweetDto);
+  }
+
+  // // delete retweet
+  @Mutation((returns) => Boolean)
+  DeleteRetweet(@Args({ name: 'id', type: () => Int }) id: number) {
+    return this.retweetService.deleteRetweet(id);
+  }
+}
--- src/module/retweet/retweet.service.ts
+++ src/module/retweet/retweet.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Retweet } from '@src/entity';
+import { CreateRetweetDto } from '@retweet/dto/create-retweet.dto';
 
 @Injectable()
-export class RetweetService {}
+export class RetweetService {
+  constructor(
+    @InjectRepository(Retweet)
+    private retweetRepostiory: Repository<Retweet>,
+  ) {}
+
+  // get all user, all retweet
+  async getAllRetweet(): Promise<Retweet[]> {
+    const data = await this.retweetRepostiory.find();
+    return data;
+  }
+
+  // get tweet
+  async getUserRetweet(id: string): Promise<Retweet[]> {
+    return await this.retweetRepostiory.find({
+      userId: id,
+    });
+  }
+
+  // get tweet
+  async getTweetRetweet(id: number): Promise<Retweet[]> {
+    return await this.retweetRepostiory.find({
+      tweetId: id,
+    });
+  }
+
+  // create retweet
+  async createRetweet(createRetweetDto: CreateRetweetDto): Promise<Retweet> {
+    const retweet = await this.retweetRepostiory.create(createRetweetDto);
+    await this.retweetRepostiory.save(retweet);
+    return retweet;
+  }
+
+  // delete retweet
+  async deleteRetweet(id: number): Promise<boolean> {
+    const result = await this.retweetRepostiory.delete(id);
+    return result.affected > 0;
+  }
+}
