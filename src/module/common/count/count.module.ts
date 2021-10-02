3f5bc34a1deeb0fe8deaa4bcc21035b7f9ad0491
miyasan_0301 <miyasan.19990301@gmail.com>
2021-10-02 19:45:32 +0900

    remove:file

--- src/module/common/count/count.module.ts
+++ src/module/common/count/count.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Retweet, Like, Comment, Follow } from '@src/entity';
-import { CountResolver } from '@common/count/count.resolver';
 import { CountService } from '@common/count/count.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Retweet, Like, Comment, Follow])],
-  providers: [CountResolver, CountService],
+  providers: [CountService],
   exports: [CountService],
 })
 export class CountModule {}
