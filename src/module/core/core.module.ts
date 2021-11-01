206762d9d8cf8fd60515ca9fc09fe9c2b354e079
miyasan_0301 <miyasan.19990301@gmail.com>
2021-11-01 17:35:21 +0900

    💉 依存関係注入

--- src/module/core/core.module.ts
+++ src/module/core/core.module.ts
@@ -1,9 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaModule } from '@core/prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { SupabaseModule } from './supabase/supabase.module';
 
 @Global()
 @Module({
-  imports: [PrismaModule],
-  exports: [PrismaModule],
+  imports: [AuthModule, PrismaModule, SupabaseModule],
+  exports: [AuthModule, PrismaModule, SupabaseModule],
 })
 export class CoreModule {}
