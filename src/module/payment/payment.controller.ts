d538704f8ba3e69ce101c1d7cbf992bf12ea9ff8
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-02 18:17:24 +0900

    ✅ test/python-shell

--- src/module/payment/payment.controller.ts
+++ src/module/payment/payment.controller.ts
@@ -1,10 +1,19 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { Payment, Prisma } from '@prisma/client';
 import { PaymentService } from '@payment/payment.service';
+import { PythonShell } from 'python-shell';
 
 @Controller('payment')
 export class PaymentController {
   constructor(private paymentsService: PaymentService) {}
+
+  @Post('test')
+  test() {
+    return PythonShell.run('index.py', null, (err, results) => {
+      if (err) throw err;
+      console.log(results);
+    });
+  }
 
   // POST - /payment
   @Post()
