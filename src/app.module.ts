736f1c0b7098d10f187104f18a86f5f0b75ca31d
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-02 16:34:03 +0900

    ♻️

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from '@core/core.module';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from '@src/module/user/user.module';
-import { VoiceModule } from './module/voice/voice.module';
-import { CreditModule } from './module/credit/credit.module';
-import { PaymentModule } from './module/payment/payment.module';
-import { ShopModule } from './module/shop/shop.module';
+import { UserModule } from '@user/user.module';
+import { VoiceModule } from '@voice/voice.module';
+import { CreditModule } from '@credit/credit.module';
+import { PaymentModule } from '@payment/payment.module';
+import { ShopModule } from '@shop/shop.module';
 
 @Module({
   imports: [
