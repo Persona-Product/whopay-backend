055a520e474a7bd69b04c80e1bb76259fd7aba39
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-02 16:52:16 +0900

    ✨ module/payment

--- src/module/payment/payment.controller.ts
+++ src/module/payment/payment.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { Payment } from '@prisma/client';
+import { Controller, Post, Body } from '@nestjs/common';
+import { Payment, Prisma } from '@prisma/client';
 import { PaymentService } from '@payment/payment.service';
 
 @Controller('payment')
 export class PaymentController {
   constructor(private paymentsService: PaymentService) {}
 
-  // GET - /payment
-  @Get(':id')
-  getPayment(@Param() id: Payment['id']): Promise<Payment> {
-    return this.paymentsService.getPayment(id);
+  // POST - /payment
+  @Post()
+  paymentTransaction(
+    @Body() createBody: Prisma.PaymentCreateInput,
+  ): Promise<Payment> {
+    return this.paymentsService.paymentTransaction(createBody);
   }
 }
--- src/module/payment/payment.service.ts
+++ src/module/payment/payment.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { Payment } from '@prisma/client';
+import { Payment, Prisma } from '@prisma/client';
 import { PrismaService } from '@core/prisma/prisma.service';
 
 @Injectable()
 export class PaymentService {
   constructor(private prisma: PrismaService) {}
 
-  async getPayment(id: Payment['id']): Promise<Payment> {
-    return this.prisma.payment.findUnique({
-      where: { id },
-    });
+  async paymentTransaction(data: Prisma.PaymentCreateInput): Promise<Payment> {
+    /*
+      1. 音声ファイルをAI実行ファイルに転送
+      2. AI実行ファイルを実行
+      3. 実行結果を取得 
+      4. 実行結果を元にユーザー情報からパスコードを取得
+      5. パスコードとリクエストのパスコードを比較
+      6. パスコードが一致したらStripeで支払い処理を実行
+      7. 支払い処理が完了したらPaymentテーブルに登録
+     */
+    return this.prisma.payment.create({ data });
   }
 }
