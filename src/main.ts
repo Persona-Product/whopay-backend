dd90f2cb9eaf9a29de94855ea502d5eaa1e3f0e7
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-02 15:42:57 +0900

    ✨ Bodyの容量++

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from '@src/app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use(bodyParser.json({ limit: '100mb' }));
+  app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 
   console.log(`NODE_ENV [ ${process.env.NODE_ENV} ]`);
   console.log(`API_SERVER_PORT [ localhost:/${process.env.PORT} ]`);
