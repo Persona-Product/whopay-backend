6b2c33c5336d37b876e7a58b25d5ab2ad3155201
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-02 15:44:01 +0900

    ✨ DI/SupabaseModule

--- src/module/voice/voice.module.ts
+++ src/module/voice/voice.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { VoiceController } from './voice.controller';
 import { VoiceService } from './voice.service';
+import { SupabaseModule } from '@core/supabase/supabase.module';
 
 @Module({
+  imports: [SupabaseModule],
   controllers: [VoiceController],
-  providers: [VoiceService]
+  providers: [VoiceService],
 })
 export class VoiceModule {}
