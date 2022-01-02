64bb75916718d7a2939f2db605ddd727e7a18f11
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-02 15:44:26 +0900

    ✨ supabase/storage

--- src/module/voice/voice.controller.ts
+++ src/module/voice/voice.controller.ts
@@ -1,14 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { VoiceService } from './voice.service';
-import { Voice } from '@prisma/client';
+import { Prisma, Voice } from '@prisma/client';
 
 @Controller('voice')
 export class VoiceController {
   constructor(private voicesService: VoiceService) {}
 
-  // GET - /voice
+  // POST - /voice
   @Post()
-  createVoice(@Body() data: any): Promise<Voice> {
-    return this.voicesService.createVoice(data);
+  createVoice(@Body() createBody: Prisma.VoiceCreateInput): Promise<Voice> {
+    return this.voicesService.createVoice(createBody);
   }
 }
--- src/module/voice/voice.service.ts
+++ src/module/voice/voice.service.ts
@@ -1,14 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { Voice, Prisma } from '@prisma/client';
 import { PrismaService } from '@core/prisma/prisma.service';
+import { SupabaseService } from '@core/supabase/supabase.service';
 
 @Injectable()
 export class VoiceService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private supabaseService: SupabaseService,
+  ) {}
 
   async createVoice(data: Prisma.VoiceCreateInput): Promise<Voice> {
+    // ファイル名
+    const date = new Date();
+    const fileName = `init/${date.getFullYear()}${
+      date.getMonth() + 1
+    }${date.getDate()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}_${
+      data.User.connect.id
+    }.m4a`;
+
+    // ファイルをデコード
+    const imageBuffer = Buffer.from(data.voiceFile, 'base64');
+
+    // アップロード処理
+    const { error } = await this.supabaseService.client.storage
+      .from('voice')
+      .upload(fileName, imageBuffer, {
+        cacheControl: '3600',
+        upsert: false,
+      });
+
+    // エラーを返す
+    if (error) throw new Error('Voice file upload failed');
+
+    // voice情報データを保存
     return await this.prisma.voice.create({
-      data,
+      data: {
+        User: { connect: { id: data.User.connect.id } },
+        voiceFile: fileName,
+      },
     });
   }
 }
