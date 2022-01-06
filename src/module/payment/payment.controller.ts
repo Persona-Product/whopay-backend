0464677a2b2fb407a8b85d88963c89c091dfd8ec
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-06 14:49:11 +0900

    ✨ controller/payment

--- src/module/payment/payment.controller.ts
+++ src/module/payment/payment.controller.ts
@@ -1,25 +1,20 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { Payment, Prisma } from '@prisma/client';
 import { PaymentService } from '@payment/payment.service';
-import { PythonShell } from 'python-shell';
+
+type PayBody = {
+  shopId: string;
+  passcode: string;
+  price: string;
+  voiceFile: string;
+};
 
 @Controller('payment')
 export class PaymentController {
   constructor(private paymentsService: PaymentService) {}
 
-  @Post('test')
-  test() {
-    return PythonShell.run('index.py', null, (err, results) => {
-      if (err) throw err;
-      console.log(results);
-    });
-  }
-
   // POST - /payment
   @Post()
-  paymentTransaction(
-    @Body() createBody: Prisma.PaymentCreateInput,
-  ): Promise<Payment> {
-    return this.paymentsService.paymentTransaction(createBody);
+  paymentTransaction(@Body() payBody: PayBody): Promise<{ result: boolean }> {
+    return this.paymentsService.paymentTransaction(payBody);
   }
 }
